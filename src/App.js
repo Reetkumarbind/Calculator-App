a2a88d407a9fa3b528bbc935f4928d00ecc5a47f
Reet <104083295+Reetkumarbind@users.noreply.github.com>
2024-03-19 00:44:40 +0530

    Welcome-App Project Folder uploaded.
    All Project Fils Added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css'; // If you have CSS styles
+
+import Calculator from './Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Calculator Application &copy; By Reet</h1>
+      <p>This is a simple calculator application using React JS and build using Create React App.</p>
+      <Calculator />
+    </div>
+  );
+}
+
+
+
+
+
+export default App;
